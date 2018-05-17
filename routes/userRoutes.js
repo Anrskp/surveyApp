54f5a733b1f739dbd5ec1090b3389827ac012f9f
anrskp <Anrskp@gmail.com>
2018-05-17 12:17:39 +0200

    ini added auth and db

---
+++ routes/userRoutes.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../config/database');
+
+// Register
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.getUserByUsername(req.body.username, (err, user) => {
+    if (err) throw err
+    if (user != null) {
+      res.json({success: false, msg: 'username already in use'})
+    } else {
+      User.getUserByEmail(req.body.email, (err, email) => {
+        if (err) throw err
+        if (email != null) {
+          res.json({success: false, msg: 'email already in use'})
+        } else {
+          User.addUser(newUser, (err, user) => {
+            if(err) {
+              res.json({success: false, msg:'Failed to register user'});
+            } else {
+              res.json({success: true, msg:'User registered'});
+            }
+          });
+        }
+      })
+    }
+  })
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      return res.json({success: false, msg: 'User not found'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch) {
+        const token = jwt.sign({data: user}, config.secret, {
+          expiresIn: 7200 // 2 hours
+        });
+
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            email: user.email,
+            username: user.username
+          }
+        });
+      } else {
+        return res.json({success: false, msg: 'Wrong password'});
+      }
+    });
+  });
+});
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+  res.json({user: req.user})
+});
+
+module.exports = router;
