eb2a4d351a5c12acf9d27082f864a8b3b9ce31bd
anrskp <Anrskp@gmail.com>
2018-06-19 13:56:15 +0200

    merge

--- node-monolith/angular-src/src/app/components/survey-data/survey-data.component.ts
+++ node-monolith/angular-src/src/app/components/survey-data/survey-data.component.ts
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { SurveyService } from '../../services/survey.service';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { FlashMessagesService } from 'angular2-flash-messages/module';
-import { ActivatedRoute } from '@angular/router';
-
-@Component({
-  selector: 'app-survey-data',
-  templateUrl: './survey-data.component.html',
-  styleUrls: ['./survey-data.component.css']
-})
-export class SurveyDataComponent implements OnInit {
-
-  surveyID : any;
-
-  constructor(
-    private flashMessage:FlashMessagesService,
-    private surveyService: SurveyService,
-    private dashboard: DashboardComponent,
-    private route: ActivatedRoute
-  ) { }
-
-  ngOnInit() {
-
-    this.route.params.subscribe(params => {
-    this.surveyID = {surveyID:params.id};
-    });
-
-    console.log(this.surveyID);
-
-    /*
-    this.surveyService.getSurveyData(this.surveyID).subscribe(data => {
-      if (data.success) {
-
-          send data to graph service
-
-
-        //this.survey = JSON.parse(data.survey);
-        //console.log(this.survey);
-      }
-      else {
-        this.flashMessage.show(data.msg, {
-          cssClass: 'alert-danger',
-          timeout: 5000
-        });
-      }
-    });
-    */
-  }
-
-}
-=======
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {SurveyService} from '../../services/survey.service';
@@ -119,4 +66,3 @@ export class SurveyDataComponent implements OnInit {
 
 
   }
->>>>>>> b5364e215a3c1cb414d3b352361f46b39ae4a5ec
